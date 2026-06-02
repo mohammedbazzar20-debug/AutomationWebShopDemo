5b9b090c7ebf8d88e980006d3aba98eb35b5ef7d
MohammedBazzar <mohammedbazzar20@gmail.com>
2026-06-02 22:39:29 +0300

    Starting Project

---
+++ Demo/src/main/java/DemoWepShop/Demo/App.java
@@ -0,0 +1,7 @@
+package DemoWepShop.Demo;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ Demo/src/test/java/DemoWepShop/Demo/AppTest.java
@@ -0,0 +1,13 @@
+package DemoWepShop.Demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
